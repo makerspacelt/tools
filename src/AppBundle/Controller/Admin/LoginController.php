49ee784c2009c81fc4750ffa2a173a1656f851d7
kulverstukas1 <kulverstukas.k@gmail.com>
2018-06-29 21:58:34 +0300

    sistemos programavimas

--- src/AppBundle/Controller/Admin/LoginController.php
+++ src/AppBundle/Controller/Admin/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Admin;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class LoginController extends Controller {
 
     /**
-     * @Route("/" name="login_page")
+     * @Route("/", name="login_page")
      */
     public function index() {
         return $this->render('admin/login.html.twig');
