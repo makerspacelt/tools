eaa4b15f649260e7685d31f3fe2e90e9e5fb0922
kulverstukas1 <kulverstukas.k@gmail.com>
2018-07-08 20:26:55 +0300

    daromas prisijungimo langas, kol kas be DB

--- src/AppBundle/Controller/Admin/AdminController.php
+++ src/AppBundle/Controller/Admin/AdminController.php
@@ -4,14 +4,25 @@ namespace AppBundle\Controller\Admin;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AdminController {
+class AdminController extends Controller {
 
     /**
      * @Route("/", name="admin_homepage")
      */
     public function index() {
+        // TODO: perkelti į konstruktorių ar kažkur, kad nereiktų kviesti kas kartą
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return new Response('SUCCESS');
-}
+    }
+
+    /**
+     * @Route("/users", name="admin_users")
+     */
+    public function users() {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return new Response('users()');
+    }
 
 }
--- src/AppBundle/Controller/Admin/LoginController.php
+++ src/AppBundle/Controller/Admin/LoginController.php
@@ -2,12 +2,12 @@
 
 namespace AppBundle\Controller\Admin;
 
-use Couchbase\RegexpSearchQuery;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class LoginController extends Controller {
 
@@ -15,6 +15,10 @@ class LoginController extends Controller {
      * @Route("/login", name="login")
      */
     public function login(Request $request, AuthenticationUtils $authenticationUtils) {
+        // TODO: reiktų sugalvoti, kaip šitą kodą vykdyti konstruktoriuje ar kažkur
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('admin_homepage');
+        }
         $error = $authenticationUtils->getLastAuthenticationError();
         return $this->render('admin/login.html.twig', array('error' => $error));
     }
