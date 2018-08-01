103523461489cf43ee20a7749c152a0d27fa5215
kulverstukas1 <kulverstukas.k@gmail.com>
2018-08-01 12:30:33 +0000

    daromas vartotojų sukūrimas

--- src/AppBundle/Controller/Admin/AdminController.php
+++ src/AppBundle/Controller/Admin/AdminController.php
@@ -8,12 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends Controller {
 
     /**
      * @Route("/", name="admin_homepage")
-     * @Security("has_role('ROLE_SUPERADMIN')")
      */
     public function index() {
         return $this->render('admin/index.html.twig');
@@ -21,12 +21,26 @@ class AdminController extends Controller {
 
     /**
      * @Route("/users", name="admin_users")
-     * @Security("has_role('ROLE_SUPERADMIN')")
      */
     public function users() {
         $repo = $this->getDoctrine()->getRepository(User::class);
         $users = $repo->findAll();
         return $this->render('admin/users.html.twig', array('users' => $users));
+    }
+
+    /**
+     * @Route("/users/addUser", name="admin_add_user")
+     */
+    public function addUser(Request $request) {
+        if ($request->request->count() == 5) {
+            // sukurti pavyko
+//            $this->addFlash('success', 'User created!');
+//            return $this->redirectToRoute('admin_users');
+
+            // sukurti nepavyko
+            
+        }
+        return $this->render('admin/add_edit_user.html.twig');
     }
 
 }
