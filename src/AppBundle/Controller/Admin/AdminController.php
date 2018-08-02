e0310ebc239b3aa79ba8b4975e50be4bb6764cc3
kulverstukas1 <kulverstukas.k@gmail.com>
2018-08-02 10:38:23 +0000

    daromas vartotojų redagavimas; padarytas stub'as

--- src/AppBundle/Controller/Admin/AdminController.php
+++ src/AppBundle/Controller/Admin/AdminController.php
@@ -39,7 +39,7 @@ class AdminController extends Controller {
      * @Route("/users/addUser", name="admin_add_user")
      */
     public function addUser(Request $request) {
-        if ($request->request->count() == 6) {
+        if ($request->request->count() == 5) {
             $user = $request->request->get('username');
             $email = $request->request->get('email');
             if (($user != null) && ($email != null)) {
@@ -48,19 +48,19 @@ class AdminController extends Controller {
                 $obj = $repo->findOneBy(array('username' => $user));
                 if ($obj != null) {
                     $this->addFlash('error', 'Username already exists!');
-                    return $this->render('admin/add_edit_user.html.twig', $request->request->all());
+                    return $this->render('admin/add_user.html.twig', $request->request->all());
                 }
                 $obj = $repo->findOneBy(array('email' => $email));
                 if ($obj != null) {
                     $this->addFlash('error', 'Email already exists!');
-                    return $this->render('admin/add_edit_user.html.twig', $request->request->all());
+                    return $this->render('admin/add_user.html.twig', $request->request->all());
                 }
                 // neegzistuoja, tai sukuriam
                 $entityManager = $this->getDoctrine()->getManager();
 
                 $postArr = $request->request->all();
                 $user = new User();
-                $user->setFullname($postArr['firstname'].' '.$postArr['lastname']);
+                $user->setFullname($postArr['fullname']);
                 $user->setUsername($postArr['username']);
                 $user->setEmail($postArr['email']);
                 $user->setPassword($this->encoder->encodePassword($user, $postArr['password']));
@@ -73,7 +73,14 @@ class AdminController extends Controller {
                 return $this->redirectToRoute('admin_users');
             }
         }
-        return $this->render('admin/add_edit_user.html.twig');
+        return $this->render('admin/add_user.html.twig');
+    }
+
+    /**
+     * @Route("/users/editUser", name="admin_edit_user")
+     */
+    public function editUser(Request $request) {
+        return $this->render('admin/edit_user.html.twig', $request->request->all());
     }
 
 }
