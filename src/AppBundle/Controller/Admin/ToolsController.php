56864d12c9d11ae4d51f985e52240844beb39945
kulverstukas1 <kulverstukas.k@gmail.com>
2018-08-20 19:05:19 +0000

    daromas įrankio įrašymas į DB ir atvaizdavimas sąraše

--- src/AppBundle/Controller/Admin/ToolsController.php
+++ src/AppBundle/Controller/Admin/ToolsController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Tool;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,7 +16,9 @@ class ToolsController extends Controller {
      * @Route("/", name="admin_tools")
      */
     public function tools() {
-        return $this->render('admin/tools/tools.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Tool::class);
+        $tools = $repo->findAll();
+        return $this->render('admin/tools/tools.html.twig', array('tools' => $tools));
     }
 
     /**
@@ -23,8 +26,28 @@ class ToolsController extends Controller {
      */
     public function addTool(Request $request) {
         $paramArr = $request->request->all();
-        if (count($paramArr) > 0) {
-            echo '<pre>'; print_r($paramArr); die();
+        if (count($paramArr) > 8) {
+            $name = $request->request->get('tool_name');
+            $model = $request->request->get('tool_model');
+            $code = $request->request->get('tool_code');
+            $descr = $request->request->get('tool_description');
+            if ($name && $model && $code && $descr) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $tool = new Tool();
+                $tool->setName($paramArr['tool_name']);
+                $tool->setModel($paramArr['tool_model']);
+                $tool->setCode($paramArr['tool_code']);
+                $tool->setDescription($paramArr['tool_description']);
+                $tool->setShopLinks($paramArr['tool_links']);
+                $tool->setOriginalPrice($paramArr['tool_price']);
+                $tool->setAcquisitionDate($paramArr['tool_date']);
+
+                $entityManager->persist($tool);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Tool created!');
+                return $this->redirectToRoute('admin_tools');
+            }
         }
         return $this->render('admin/tools/add_tool.html.twig');
     }
--- src/AppBundle/Entity/Tool.php
+++ src/AppBundle/Entity/Tool.php
@@ -86,4 +86,114 @@ class Tool implements \Serializable {
             ) = unserialize($serialized, array('allowed_classes' => false));
     }
 
+    #=====================================================
+
+    /**
+     * @return mixed
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModel() {
+        return $this->model;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode() {
+        return $this->code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription() {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShopLinks() {
+        return $this->shopLinks;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOriginalPrice() {
+        return $this->originalPrice;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAcquisitionDate() {
+        return $this->acquisitionDate;
+    }
+
+    #=====================================================
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $model
+     */
+    public function setModel($model) {
+        $this->model = $model;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code) {
+        $this->code = $code;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    /**
+     * @param mixed $shopLinks
+     */
+    public function setShopLinks($shopLinks) {
+        $this->shopLinks = $shopLinks;
+    }
+
+    /**
+     * @param mixed $originalPrice
+     */
+    public function setOriginalPrice($originalPrice) {
+        $this->originalPrice = $originalPrice;
+    }
+
+    /**
+     * @param mixed $acquisitionDate
+     */
+    public function setAcquisitionDate($acquisitionDate) {
+        $this->acquisitionDate = $acquisitionDate;
+    }
+
+    #=====================================================
 }
