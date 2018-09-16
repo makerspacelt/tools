70d8ce168d6a29432a5b59bcb8740068c6187580
kulverstukas1 <kulverstukas.k@gmail.com>
2018-09-16 17:54:55 +0000

    padarytas etikečių (label) atvaizdavimas pagrindiniam puslapyje

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\ToolTag;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,7 +12,10 @@ class IndexController extends Controller {
      * @Route("/", name="index_page")
      */
     public function index() {
-        return $this->render('index.html.twig');
+        // get tags
+        $repo = $this->getDoctrine()->getRepository(ToolTag::class);
+        $tags = $repo->findAll();
+        return $this->render('index.html.twig', array('tags' => $tags));
     }
 
 }
