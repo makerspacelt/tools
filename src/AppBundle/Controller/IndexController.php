e47d207e0ab31b327b28b947ed317253a547797b
kulverstukas1 <kulverstukas.k@gmail.com>
2018-09-20 17:37:13 +0000

    padarytas įrankių atvaizdavimas pagrindiniam puslapyje

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Tool;
 use AppBundle\Entity\ToolTag;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,7 +16,12 @@ class IndexController extends Controller {
         // get tags
         $repo = $this->getDoctrine()->getRepository(ToolTag::class);
         $tags = $repo->findAll();
-        return $this->render('index.html.twig', array('tags' => $tags));
+
+        // get tools
+        $repo = $this->getDoctrine()->getRepository(Tool::class);
+        $tools = $repo->findAll();
+
+        return $this->render('index.html.twig', array('tags' => $tags, 'tools' => $tools));
     }
 
 }
