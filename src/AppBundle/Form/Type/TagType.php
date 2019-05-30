1d06e6dbc68143b64787dd814c0e638ce7232b5e
kulverstukas1 <kulverstukas.k@gmail.com>
2019-05-30 20:16:37 +0000

    padarytas tag'ų išsaugojimas kuriant naują įrankį

---
+++ src/AppBundle/Form/Type/TagType.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace AppBundle\Form\Type;
+
+use AppBundle\Form\DataTransformer\TagTransformer;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TagType extends AbstractType {
+
+    private $em;
+
+    public function __construct(EntityManager $em) {
+        $this->em = $em;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add(
+            'tags', TextType::class, ['required' => false, 'attr' => ['class' => 'tagsinput']]
+        )->addModelTransformer(new TagTransformer($this->em));
+    }
+
+}
